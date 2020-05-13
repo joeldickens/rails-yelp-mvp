d6311e4a3c486d8a07f6ee01d8c572781d5e4026
Joel Dickens <joeladickens@gmail.com>
2020-05-13 14:43:40 +0100

    created models

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,7 +5,8 @@
 
 require("@rails/ujs").start()
 require("turbolinks").start()
-require("channels")
+require("channels");
+import 'bootstrap';
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
