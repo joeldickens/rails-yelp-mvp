c4e37fb1a6a8bd91d23c60e048163d45352a2302
Joel Dickens <joeladickens@gmail.com>
2020-05-13 22:14:20 +0100

    not done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+restaurants = Restaurant.create([{ name: 'The Bao', address: 'St Marks', phone_number: '2125550000', category: 'Chinese' }, { name: 'Il Corallo', address: 'Price', phone_number: '2125550001', category: 'Italian' }, { name: 'SoHo Sushi', address: 'SoHo', phone_number: '2125550002', category: 'Japanese' }, { name: 'Paris', address: 'Paris', phone_number: '2125550003', category: 'French' }, { name: 'Fries', address: 'Deals Gateway', phone_number: '2125550004', category: 'Belgian' }])
